94506996cef2152b4978be2c86d9e050aecffc8b
unknown <mertyasin1644@gmail.com>
2023-11-24 10:43:20 +0300

    sepetten aynı ürünü ekletmeme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const row = document.querySelector('.row')
-// const span = document.querySelector('#span')
+const span = document.querySelector('#span')
+// const row = document.querySelector('.row')
 
 
 let sepet = []
@@ -11,8 +11,7 @@ if (localItem) {
     // console.log(sepet)
     span.textContent = sepet.length
 }
-
-
+// localStorage.clear()
 // console.log(urunler)
 
 console.log(window.location.href)
@@ -21,104 +20,124 @@ console.log(sepet.length)
 if (window.location.href == 'http://127.0.0.1:5500/index.html') {
     const row = document.querySelector('.row')
 
-urunler.forEach((urun)=> {
+    urunler.forEach((urun) => {
 
-    // console.log(urun)
+        // console.log(urun)
 
-    urun.quantity = 1
+        urun.quantity = 1
 
-    // console.log(urun)
+        // console.log(urun)
 
-    const col = document.createElement('div')
-    col.classList.add('col-lg-3', 'col-sm-6', 'col-12')
+        const col = document.createElement('div')
+        col.classList.add('col-lg-3', 'col-sm-6', 'col-12')
 
-    const parentDiv = document.createElement('div')
-    parentDiv.style.width = '100%'
-    parentDiv.style.height = '420px'
-    parentDiv.style.border = '1px solid black'
+        const parentDiv = document.createElement('div')
+        parentDiv.style.width = '100%'
+        parentDiv.style.height = '420px'
+        parentDiv.style.border = "1px solid black"
 
-    const imgDiv = document.createElement('div')
-    imgDiv.style.width = '100%'
-    imgDiv.style.height = '200px'
+        const imgDiv = document.createElement('div')
+        imgDiv.style.width = "100%"
+        imgDiv.style.height = "200px"
 
-    const img = document.createElement('img')
-    img.src = urun.fotoğraf
-    img.style.width = '100%'
-    img.style.height = '100%'
+        const img = document.createElement('img')
+        img.src = urun.fotoğraf
+        img.style.width = "100%"
+        img.style.height = "100%"
 
-    const cardBody = document.createElement('div')
-    cardBody.style.width = '100%'
-    cardBody.style.height = '220px'
+        const cardBody = document.createElement('div')
+        cardBody.style.width = "100%"
+        cardBody.style.height = "220px"
 
-    const baslik = document.createElement('h5')
-    baslik.textContent = urun.isim
+        const baslik = document.createElement('h5')
+        baslik.textContent = urun.isim
 
-    const aciklama = document.createElement('p')
-    aciklama.textContent = `${urun.açıklama} - ${urun.fiyat}$`
+        const aciklama = document.createElement('p')
+        aciklama.textContent = `${urun.açıklama} - ${urun.fiyat}$`
 
-    const btn = document.createElement('button')
-    btn.classList.add('btn', 'btn-dark')
-    btn.textContent = 'Sepete Ekle'
+        const btn = document.createElement('button')
+        btn.classList.add('btn', 'btn-dark')
+        btn.textContent = 'Sepete Ekle'
 
-    btn.addEventListener('click', () => {
-        sepet.push(urun)
+        btn.addEventListener('click', () => {
 
-        localStorage.setItem('sepet', JSON.stringify(sepet))
-        span.textContent = sepet.length
-        
+            console.log(sepet.length)
+
+            let found = false
+
+            if (sepet.length == 0) {
+                sepet.push(urun)
+
+                found = true
+            } else {
+                sepet.forEach(sepetUrunu => {
+                    // console.log(sepetUrunu.isim == urun.isim)
+                    if (sepetUrunu.isim == urun.isim) {
+                        sepetUrunu.quantity++
+                        found = true
+                    }
+
+                })
+            }
+
+            if (!found) {
+                sepet.push(urun)
+            }
+
+            localStorage.setItem('sepet', JSON.stringify(sepet))
+            span.textContent = sepet.length
+        })
+
+        cardBody.append(baslik)
+        cardBody.append(aciklama)
+        cardBody.append(btn)
+
+        imgDiv.append(img)
+
+        parentDiv.append(imgDiv)
+        parentDiv.append(cardBody)
+
+        col.append(parentDiv)
+
+        row.append(col)
+
     })
-
-    cardBody.append(baslik)
-    cardBody.append(aciklama)
-    cardBody.append(btn)
-
-    imgDiv.append(img)
-
-    parentDiv.append(imgDiv)
-    parentDiv.append(cardBody)
-
-    col.append(parentDiv)
-
-    row.append(col)
-
-
-
-})
 } else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
     const container = document.querySelector('.container')
+
     if (sepet.length == 0) {
         const h4 = document.createElement('h4')
         h4.textContent = 'Sepetinizde Ürün Yok...'
+
         container.append(h4)
-
     } else {
-        //! sepetimin uzunluğu 0 dan büyük olduğu sürece buradaki kodlar çalışacak
-
+        //! Sepetimin uzunluğu 0 dan büyük olduğu sürece burdaki kodlar çalışacak
         sepet.forEach(urun => {
             const div = document.createElement('div')
-            div.style.width = '100%'
+            div.style.width = "100%"
             div.style.height = '200px'
-            div.style.border = '1px solid black'
+            div.style.border = "1px solid black"
             div.classList.add('d-flex', 'align-items-center', 'justify-content-between', 'mt-2')
 
             const imgDiv = document.createElement('div')
-            imgDiv.style.width = '25%'
-            imgDiv.style.height = '100%'
+            imgDiv.style.width = "25%"
+            imgDiv.style.height = "100%"
 
             const img = document.createElement('img')
             img.src = urun.fotoğraf
-            img.style.width = '100%'
-            img.style.height = '100%'
+            img.style.width = "100%"
+            img.style.height = "100%"
 
             const baslik = document.createElement('h3')
             baslik.textContent = urun.isim
 
             const price = document.createElement('p')
-            price.textContent = urun.fiyat + '$'
+            let urununFiyati = urun.fiyat * urun.quantity
+            price.textContent = urununFiyati + '$'
             price.style.fontWeight = 'bold'
 
             const kacTane = document.createElement('div')
-            //? bir şeye ihtiyac olursa ekleriz
+            //? bir şeye ihtiyaç olursa ekleriz
             kacTane.classList.add('d-flex', 'gap-3')
 
             const sayi = document.createElement('p')
@@ -130,31 +149,38 @@ urunler.forEach((urun)=> {
             azaltBtn.classList.add('btn', 'btn-light')
 
             const arttirBtn = document.createElement('button')
-            arttirBtn.textContent = '+'
+            arttirBtn.textContent = "+"
             arttirBtn.classList.add('btn', 'btn-light')
 
-            azaltBtn.addEventListener('click', function() {
-               if (urun.quantity >= 1) {
-                urun.quantity--
-                sayi.textContent = urun.quantity
-                 if (urun.quantity === 0) {
-                // console.log(this.parentElement.parentElement)
-                this.parentElement.parentElement.remove()
+            azaltBtn.addEventListener('click', function () {
+                if (urun.quantity >= 1) {
+                    urun.quantity--
+                    sayi.textContent = urun.quantity
 
-                let urunIndex = sepet.indexOf(urun)
 
-                sepet.splice(urunIndex, 1)
+                    if (urun.quantity == 0) {
+                        // console.log(this.parentElement.parentElement)
+                        this.parentElement.parentElement.remove()
 
-                localStorage.setItem('sepet', JSON.stringify(sepet))
-               }
+                        //! LocalStorage'dan silme
+                        let urunIndex = sepet.indexOf(urun)
 
-            }
+                        sepet.splice(urunIndex, 1)
 
+                        localStorage.setItem('sepet', JSON.stringify(sepet))
+                        //! LocalStorage'dan silme
+
+                    }
+                }
             })
 
             arttirBtn.addEventListener('click', () => {
                 urun.quantity++
                 sayi.textContent = urun.quantity
+
+                // urun.fiyat += urun.quantity
+                // price.textContent = urun.fiyat
+
             })
 
             imgDiv.append(img)
@@ -170,8 +196,9 @@ urunler.forEach((urun)=> {
 
             container.append(div)
 
-            
         })
+
     }
+
 }
 // localStorage.clear()
